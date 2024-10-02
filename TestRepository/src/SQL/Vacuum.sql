a3958333dd4a40dbaea03c24d4e50370b09bb6af
eoghks <zhfldk7316@naver.com>
2024-10-02 21:34:30 +0900

    1. anti wraapound 관련 파라미터 조회문 수정

--- TestRepository/src/SQL/Vacuum.sql
+++ TestRepository/src/SQL/Vacuum.sql
@@ -2,4 +2,4 @@
 select name,setting from pg_settings where name in ('autovacuum_vacuum_scale_factor', 'autovacuum_vacuum_threshold');
 
 -- autovacuum(Anti Wraparound)관련 파라미터 조회
-select name,setting from pg_settings where name like '%freeze%';
+select name,setting from pg_settings where name like '%freeze%' and name not like '%mul%';
